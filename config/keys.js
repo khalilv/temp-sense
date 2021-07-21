39fb75b657dcbb4b2c1cd97fe7d116092b46eb3a
Khalil Virji <khalil_virji@hotmail.com>
2021-07-21 16:59:44 -0400

    proper mongo config

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-const password = 'DZfsZaxskA9Pyjto'
-const username = 'node-user'
-module.exports = {
-    MongoURI : 'mongodb+srv://' + username + ':' + password + '@main.asvis.mongodb.net/TemperatureMonitoringDB?retryWrites=true&w=majority',
-}
